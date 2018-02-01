8323892d3db54decee89f8a45c5633e84fa4a0b4
Patricio-Gtz <p.gutierrez.itunes@gmail.com>
2018-01-31 20:29:54 -0600

    Programa todavia no acabado, falta verificar

--- funciones.h
+++ funciones.h
@@ -1,10 +1,24 @@
+/*
+ * @brief   [Funciones para el programa de tarea2]
+ * @author  [Diego Patricio Gutierrez]
+ * @date    [1/febrero/2018]
+ * @file    [tarea2.c funciones.h]
+ */
 #include <stdio.h>
 #include <math.h>
 
+// Prototipos de funciones
 double serie(int repet);
 double factorial(int fac);
 
+/**
+ * Calcula la serie de Taylor y la guarda en un archivo llamado result.txt
+ * @param   int repet [numero de iteraciones elegidas por el usuario]
+ * @return [0 si hay error, total si todo sale bien]
+ */
 double serie(int repet) {
+
+  FILE *file = fopen("result.txt", "w");
 
   int grados = 360;
   double radianes = 0;
@@ -12,19 +26,34 @@ double serie(int repet) {
   double total = 0;
   int i = 0;
 
-  radianes = (grados * M_PI) / 180;
+  if(!file) {
+    printf("Error en el archivo...");
+    return 0;
+  }
+
+  // Conversion de grado a radianes
+  radianes = ((grados * M_PI) / 180);
 
   for(i = 0; i <= repet; i++) {
-
+    // Calcula factorial
     fac = factorial(2 * i + 1);
+    // Total del resultado
     total += (pow(-1, i) * (pow(radianes, (2 * i) + 1) / fac));
-
-    printf("x = %d para y = %f\n", i, total);
+    fprintf(file, "%d\t%f\n", i, total);
   }
+
+  fclose(file);
+
   return total;
 }
 
-
+/**
+ * Calcula el factorial de manera recursiva
+ * @param     int fac [Recibe el numero del factorial a calcular]
+ * @return     [1 si el numero es 0 ó 1, si es otro el numero calcula
+ *              el factorial]
+ * @obtenido [https://stackoverflow.com/questions/7212158/taylor-series-in-c]
+ */
 double factorial(int fac) {
 
   if(fac == 0 || fac == 1) {
--- tarea2.c
+++ tarea2.c
@@ -1,15 +1,32 @@
+/*
+ * @brief   [Programa que calcula la serie de Taylor para un seno y lo guarda
+ *           en un archivo .txt]
+ * @author  [Diego Patricio Gutierrez]
+ * @date    [1/febrero/2018]
+ * @file    [tarea2.c funciones.h]
+ */
 #include "funciones.h"
 
-//extern double factorial(int fac);
+// Prototipos de funciones
+extern double factorial(int fac);
 extern double serie(int repet);
 
+/*
+ * Recibe los datos desde terminal
+ * @param  argc [variable que define cuantos datos a ingresado el usuario en
+ *               terminal]
+ * @param  argv [dato obtenido desde terminal]
+ * @return      [0 si todo funciono bien]
+ */
 int main(int argc, char const *argv[]) {
 
   int num;
 
+  // Solo deja recibir un numero
   if(argc > 2) {
     printf("Demasiados argumentos...\n");
   } else {
+    // Convierte en numero ingresado de un char a un int
     sscanf(argv[1], "%d", &num);
     serie(num);
   }
